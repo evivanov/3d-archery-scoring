1e43d4aca33e65a3cf82e8fbeb8602f49a635ffc
Evgenii Ivanov <31668382+evivanov@users.noreply.github.com>
2025-07-28 21:07:04 +0200

    moved score rules to a separate file

---
+++ scores.js
@@ -0,0 +1,16 @@
+// scores.js
+// Map arrows hit and result type to points
+window.scores = {
+  Kill: {
+    "0": 0,  // Miss
+    "1": 20,
+    "2": 14,
+    "3": 8
+  },
+  Wound: {
+    "0": 0,
+    "1": 17,
+    "2": 11,
+    "3": 5
+  }
+};
