64e349ffbf0ba4ccb40689829259e517ccd90b50
evivanov <evivanov@users.noreply.github.com>
2025-07-28 08:59:11 +0000

    deploy: 1dee005729c6ce62b0c7a22fe4b3f794b3b08ea9

---
+++ env.js
@@ -0,0 +1,8 @@
+window._env_ = {
+  apiKey: "AIzaSyDIt4-lWaAEL9q5_Vz9tUbOkmBJ2ot3aFc",
+  authDomain: "d-archery-scoring.firebaseapp.com",
+  projectId: "d-archery-scoring",
+  storageBucket: "d-archery-scoring.firebasestorage.app",
+  messagingSenderId: "428789778749",
+  appId: "1:428789778749:web:e717b1395e3c64e29f5d54"
+};
