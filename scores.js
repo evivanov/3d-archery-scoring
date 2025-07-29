fd49d496ae938a710650c80367b534753b30c71a
evivanov <evivanov@users.noreply.github.com>
2025-07-29 07:59:07 +0000

    deploy: 1c3806428fdcd5a60d2f32a210b32fbba2e45576

--- scores.js
+++ scores.js
@@ -2,15 +2,15 @@
 // Map arrows hit and result type to points
 window.scores = {
   1: { // Kill (code 1)
-    "0": 0,  // Miss
-    "1": 20,
-    "2": 14,
-    "3": 8
+    0: 0,  // Miss
+    1: 20,
+    2: 14,
+    3: 8
   },
   2: { // Wound (code 2)
-    "0": 0,
-    "1": 17,
-    "2": 11,
-    "3": 5
+    0: 0,
+    1: 17,
+    2: 11,
+    3: 5
   }
 };
